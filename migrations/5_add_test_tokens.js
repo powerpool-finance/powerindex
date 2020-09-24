2fd2038a963550425425f221aacb25eae054fd7e
defi-dev <defi.dev@protonmail.com>
2020-09-24 22:36:04 +0200

    change test pool name and symbol

--- migrations/5_add_test_tokens.js
+++ migrations/5_add_test_tokens.js
@@ -70,8 +70,8 @@ module.exports = function(deployer, network, accounts) {
             //     swapFee: 0.05,
             //     miningVotes: true
             // },{
-            name: 'Power Index 1',
-            symbol: 'PI1',
+            name: 'Power Pool Token',
+            symbol: 'PPT',
             tokens: [lendToken.address, yfiToken.address, compToken.address, umaToken.address, mkrToken.address, uniToken.address, crvToken.address, snxToken.address],
             balances: [50, 10, 100, 200, 150, 75, 125, 60],
             denorms: [6.25, 6.25, 6.25, 6.25, 6.25, 6.25, 6.25, 6.25],
