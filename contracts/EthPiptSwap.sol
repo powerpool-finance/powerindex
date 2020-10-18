48cf93498d175ec130d7cf8e7dcb8ca0fbc3131e
defi-dev <defi.dev@protonmail.com>
2020-10-18 22:46:06 +0200

    EthPiptSwap draft

---
+++ contracts/EthPiptSwap.sol
@@ -0,0 +1,144 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./interfaces/BPoolInterface.sol";
+import "./interfaces/TokenInterface.sol";
+import "./uniswapv2/interfaces/IUniswapV2Pair.sol";
+
+contract ExchangeProxy {
+    using SafeMath for uint256;
+    using SafeERC20 for TokenInterface;
+
+    address private constant ETH_ADDRESS = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+
+    TokenInterface public weth;
+    BPoolInterface public pipt;
+    uint256 public fee;
+    address public feePayout;
+    address public feeManager;
+
+    mapping(address => address) uniswapEthPairByTokenAddress;
+
+    constructor(
+        address _weth,
+        address _pipt,
+        address _uniswapRouter,
+        uint256 _fee,
+        address _feePayout,
+        address _feeManager
+    ) public {
+        weth = TokenInterface(_weth);
+        pipt = BPoolInterface(_pipt);
+        fee = _fee;
+        fee = _fee;
+        feePayout = _feePayout;
+        feeManager = _feeManager;
+    }
+
+    receive() external payable {
+        (, uint256 valueToSwap) = takeEthFee(msg.value);
+
+        (
+            uint256[] memory tokensInPipt,
+            uint256[] memory ethInUniswap,
+            uint256 poolAmountOut
+        ) = getEthAndTokensIn(valueToSwape, tokens);
+        swapEthToPipt(tokensInPipt, ethInUniswap, poolAmountOut);
+    }
+
+    function swapEthToPipt(uint256[] memory tokensInPipt, uint256[] memory ethInUniswap, uint256 poolAmountOut) public {
+        weth.deposit.value(msg.value)();
+
+        (uint256 valueFe, uint256 valueToSwap) = takeEthFee(msg.value);
+
+        address[] memory tokens = pipt.getCurrentTokens();
+        uint256 len = tokens.length;
+
+        uint256 totalEthSwap = 0;
+        for(uint256 i = 0; i < len; i++) {
+            IUniswapV2Pair tokenPair = uniswapPairFor(tokens[i]);
+            weth.approve(address(tokenPair), ethInUniswap[i]);
+            tokenPair.swap(tokensInPipt[i], uint(0), address(this), new bytes(0));
+            //TODO: add swap check
+            totalEthSwap = totalEthSwap.add(ethInUniswap[i]);
+        }
+
+        pipt.joinPool(poolAmountOut, tokensInPipt);
+
+        pipt.transfer(msg.sender, poolAmountOut);
+        uint256 ethDiff = valueToSwap.sub(totalEthSwap);
+        if (ethDiff > 0) {
+            weth.safeTransfer(msg.sender, ethDiff);
+        }
+
+        weth.safeTransfer(feePayout, valueFee);
+    }
+
+    function getEthAndTokensIn(uint256 _ethValue, address[] memory tokens) public returns(
+        uint256[] memory tokensInPipt,
+        uint256[] memory ethInUniswap,
+        uint256 poolOut
+    ) {
+        uint256 piptTotalSupply = pipt.totalSupply();
+        uint256 piptTotalWeight = pipt.getTotalDenormalizedWeight();
+        uint256 swapFee = pipt.getSwapFee();
+
+        uint256 len = tokens.length;
+
+        uint256[] memory poolOuts = new uint256[](len);
+        uint256 totalPoolOut = 0;
+
+        uint256 firstTokenBalance = pipt.getBalance(tokens[0]);
+
+        // get pool out for 1 ether as 100% for calculate shares
+        uint256 totalPoolOut = piptTotalSupply.mul(1 ether).div(firstTokenBalance);
+        uint256 poolRatio = totalPoolOut.mul(1 ether).div(piptTotalSupply);
+
+        // get shares and eth required for each share
+        uint256[] memory tokensShares = new uint256[](len);
+        uint256[] memory ethRequired = new uint256[](len);
+        uint256 totalEthRequired = 0;
+        for (uint256 i = 0; i < len; i++) {
+            tokensShares[i] = poolRatio.mul(pipt.getBalance(tokens[i])).div(1 ether);
+            IUniswapV2Pair tokenPair = uniswapPairFor(tokens[i]);
+            (uint256 tokenReserve, uint256 ethReserve,) = tokenPair.getReserves();
+            ethRequired[i] = getAmountIn(tokensShares[i], tokenReserve, ethReserve);
+            totalEthRequired = totalEthRequired.add( ethRequired[i]);
+        }
+
+        // calculate eth and tokensIn based on shares and normalize if totalEthRequired more than 100%
+        tokensInPipt = new uint256[](len);
+        ethInUniswap = new uint256[](len);
+        for (uint256 i = 0; i < len; i++) {
+            uint256 ethShare;
+            if (totalEthRequired > 1 ether) {
+                ethShare = ethRequired[i].mul(1 ether).div(totalEthRequired);
+            } else {
+                ethShare = ethRequired[i];
+            }
+            ethInUniswap[i] = _ethValue.mul(ethShare).div(1 ether);
+            tokensInPipt[i] = tokensShares[i].mul(ethShare).div(1 ether);
+        }
+
+        poolOut = piptTotalSupply.mul(tokensInPipt[0]).div(firstTokenBalance);
+    }
+
+    function uniswapPairFor(address token) internal returns(IUniswapV2Pair) {
+        return IUniswapV2Pair(uniswapEthPairByTokenAddress[token]);
+    }
+
+    function takeEthFee(uint256 ethValue) public returns(uint256 ethFee, uint256 ethAfterFee) {
+        ethFee = ethValue.mul(fee).div(1 ether);
+        ethAfterFee = ethValue.sub(valueFee);
+    }
+
+    // given an output amount of an asset and pair reserves, returns a required input amount of the other asset
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
+        require(amountOut > 0, 'UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT');
+        require(reserveIn > 0 && reserveOut > 0, 'UniswapV2Library: INSUFFICIENT_LIQUIDITY');
+        uint numerator = reserveIn.mul(amountOut).mul(1000);
+        uint denominator = reserveOut.sub(amountOut).mul(997);
+        amountIn = (numerator / denominator).add(1);
+    }
+}
---
+++ contracts/interfaces/BPoolInterface.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.6.12;
+
+abstract contract BPoolInterface {
+    function swapExactAmountIn(address, uint, address, uint, uint) external virtual returns (uint, uint);
+    function swapExactAmountOut(address, uint, address, uint, uint) external virtual returns (uint, uint);
+    function calcInGivenOut(uint, uint, uint, uint, uint, uint) public pure virtual returns (uint);
+    function getDenormalizedWeight(address) external view virtual returns (uint);
+    function getBalance(address) external view virtual returns (uint);
+    function getSwapFee() external view virtual returns (uint);
+
+    function getCurrentTokens() external view returns (address[] memory tokens);
+}
