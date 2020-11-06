bed2aa88935cc586b5e16323a2cd389c24bcda9b
Poli Paul <polipaul777@hotmail.com>
2020-11-06 14:54:59 +0200

    Cleanup imports

--- contracts/PiBPoolController.sol
+++ contracts/PiBPoolController.sol
@@ -4,10 +4,6 @@ pragma solidity 0.6.12;
 import "./PiBPoolAbstractController.sol";
 import "./WrappedPiErc20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 
 contract PiBPoolController is PiBPoolAbstractController {
 
