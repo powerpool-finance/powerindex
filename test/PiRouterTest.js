1d9180488f2a31cfa73eb681d94245a6f6d61ee5
Defi Dev <defi.dev@protonmail.com>
2020-11-10 16:24:13 +0300

    crv voting test

--- test/PiRouterTest.js
+++ test/PiRouterTest.js
@@ -8,6 +8,8 @@ const PiRouter = artifacts.require('PiRouter');
 const WrappedPiErc20 = artifacts.require('WrappedPiErc20');
 const PiSimpleRouter = artifacts.require('PiSimpleRouter');
 const PoolRestrictions = artifacts.require('PoolRestrictions');
+const contract = require('@truffle/contract');
+const fs = require('fs');
 
 MockERC20.numberFormat = 'String';
 MockErc20Migrator.numberFormat = 'String';
@@ -29,6 +31,41 @@ describe('PiBPoolController', () => {
         const token = await MockERC20.new('My Token 3', 'MT3', ether('1000000'));
         const router = await PiSimpleRouter.new();
         const wrapper = await WrappedPiErc20.new(token.address, router.address, 'WToken', 'WTKN');
+        const poolRestrictions = await PoolRestrictions.new();
+        const router2 = await PiRouter.new(poolRestrictions.address);
+
+        assert.equal(await router.owner(), minter);
+
+        await token.transfer(alice, ether('100'));
+        await token.approve(wrapper.address, ether('100'), { from: alice });
+        await wrapper.deposit(ether('100'), { from: alice });
+
+        assert.equal(await wrapper.totalSupply(), ether('100'));
+        assert.equal(await wrapper.balanceOf(alice), ether('100'));
+
+        await expectRevert.unspecified(wrapper.changeRouter(bob));
+        await router.migrateWrappedTokensToNewRouter([wrapper.address], router2.address);
+
+        assert.equal(await wrapper.router(), router2.address);
+
+        await wrapper.approve(wrapper.address, ether('100'), { from: alice });
+        await wrapper.withdraw(ether('100'), { from: alice });
+
+        assert.equal(await wrapper.totalSupply(), ether('0'));
+        assert.equal(await wrapper.balanceOf(alice), ether('0'));
+        assert.equal(await token.balanceOf(alice), ether('100'));
+    });
+
+    it('should correctly vote for CRV voting', async () => {
+        const CRV = await MockERC20.new('CRV', 'CRV', ether('1000000'));
+        const CrvVoting = contract({
+            abi: fs.readFileSync('contracts/test/CrvVotingAbi.json'),
+            bytecode: fs.readFileSync('contracts/test/CrvVoting').replace('d533a949740bb3306d119cc777fa900ba034cd52', CRV.address.replace('0x', ''))
+        });
+        CrvVoting.setProvider(web3.currentProvider);
+
+        const router = await PiSimpleRouter.new();
+        const wrapper = await WrappedPiErc20.new(CRV.address, router.address, 'WToken', 'WTKN');
         const poolRestrictions = await PoolRestrictions.new();
         const router2 = await PiRouter.new(poolRestrictions.address);
 
