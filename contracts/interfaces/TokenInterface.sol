90eab62f6e052a3cef6ebde3157d15954bde9d9b
Poli Paul <polipaul777@hotmail.com>
2020-11-13 19:08:44 +0200

    Fix compiler warnings

--- contracts/interfaces/TokenInterface.sol
+++ contracts/interfaces/TokenInterface.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface TokenInterface is IERC20 {
-  function deposit() external payable virtual;
+  function deposit() external payable;
 
-  function withdraw(uint256) external virtual;
+  function withdraw(uint256) external;
 }
