e4f78f7ab38327e053b5ee0bf6a284a1869d03d0
defi-dev <defi.dev@protonmail.com>
2020-11-20 11:20:24 +0200

    prevent same tx origin in Bpool

---
+++ contracts/test/MockBPoolClient.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+
+import "../EthPiptSwap.sol";
+
+contract MockBPoolClient {
+
+  function callBPoolTwice(
+    EthPiptSwap piptSwap
+  )
+    external
+    returns ()
+  {
+    piptSwap.swapEthToPipt{value: msg.value / 2}(0.1 ether);
+    piptSwap.swapEthToPipt{value: msg.value / 2}(0.1 ether);
+  }
+}
