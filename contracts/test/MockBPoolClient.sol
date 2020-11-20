4857325d672e5f988c003eb59ff757e6ffc5c979
defi-dev <defi.dev@protonmail.com>
2020-11-20 13:59:39 +0200

    test BPool same tx origin

--- contracts/test/MockBPoolClient.sol
+++ contracts/test/MockBPoolClient.sol
@@ -5,9 +5,10 @@ pragma solidity 0.6.12;
 import "../EthPiptSwap.sol";
 
 contract MockBPoolClient {
+  receive() external payable {}
 
   function callBPoolTwice(EthPiptSwap piptSwap) external payable {
-    piptSwap.swapEthToPipt{value: msg.value / 2}(0.1 ether);
-    piptSwap.swapEthToPipt{value: msg.value / 2}(0.1 ether);
+    piptSwap.swapEthToPipt{ value: msg.value / 2 }(0.1 ether);
+    piptSwap.swapEthToPipt{ value: msg.value / 2 }(0.1 ether);
   }
 }
