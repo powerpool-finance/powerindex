4c245d7641594d360b0b2587e7026afc3a453840
defi-dev <defi.dev@protonmail.com>
2020-12-03 22:04:34 +0200

    deploy pool restrictions

---
+++ tasks/deployPoolRestrictions.js
@@ -0,0 +1,38 @@
+require('@nomiclabs/hardhat-truffle5');
+
+task('deploy-pool-restrictions', 'Deploy PoolRestrictions').setAction(async () => {
+  const PoolRestrictions = await artifacts.require('PoolRestrictions');
+
+  const { web3 } = PoolRestrictions;
+
+  const [deployer] = await web3.eth.getAccounts();
+  console.log('deployer', deployer);
+  const sendOptions = { from: deployer };
+
+  const delegateSig = '0x5c19a95c'; //delegate(address)
+
+  const admin = '0xb258302c3f209491d604165549079680708581cc';
+
+  const poolRestrictions = await PoolRestrictions.new(sendOptions);
+
+  await poolRestrictions.setVotingSignaturesForAddress(
+    '0xc00e94cb662c3520282e6f5717214004a7f26888', //COMP
+    true,
+    [delegateSig] //delegate(address),
+    [true]
+  );
+
+  await poolRestrictions.setVotingSignaturesForAddress(
+    '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', //UNI
+    true,
+    [delegateSig] //delegate(address),
+    [true]
+  );
+
+  await poolRestrictions.setTotalRestrictions(
+    ['0x26607aC599266b21d13c7aCF7942c7701a8b699c'],
+    ['0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff']
+  );
+
+  await poolRestrictions.transferOwnership(admin);
+});
