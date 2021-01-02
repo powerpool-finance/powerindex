e964958c84f499e4159ba989e78d3f49453f3233
defi-dev <defi.dev@protonmail.com>
2021-01-02 19:25:22 +0200

    test claim and distribute yfi rewards

--- tasks/deployYearnRouter.js
+++ tasks/deployYearnRouter.js
@@ -11,6 +11,7 @@ task('deploy-yearn-router', 'Deploy Yearn Router')
     const PowerIndexPool = await artifacts.require('PowerIndexPool');
     const PowerIndexRouterFactory = await artifacts.require('YearnPowerIndexRouterFactory');
     const PowerIndexRouter = await artifacts.require('YearnPowerIndexRouter');
+    const MockERC20 = await artifacts.require('MockERC20');
 
     const {impersonateAccount, callContract, increaseTime, forkReplacePoolTokenWithNewPiToken} = require('../test/helpers');
     const {buildBasicRouterArgs, buildYearnRouterArgs} = require('../test/helpers/builders');
@@ -22,6 +23,7 @@ task('deploy-yearn-router', 'Deploy Yearn Router')
     const sendOptions = {from: deployer};
 
     const yfiAddr = '0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e';
+    const yCrvAddr = '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8';
     const votingAddr = '0xBa37B002AbaFDd8E89a1995dA52740bbC013D992';
     const stakingAddr = '0xBa37B002AbaFDd8E89a1995dA52740bbC013D992';
     const admin = '0xb258302c3f209491d604165549079680708581cc';
@@ -66,10 +68,10 @@ task('deploy-yearn-router', 'Deploy Yearn Router')
         reserveRatio: ether(0.8).toString(),
         rebalancingInterval: '3600',
         pvp: '0xd132973eaebbd6d7ca7b88e9170f2cca058de430',
-        pvpFee: '0',
+        pvpFee: ether(0.003),
         rewardPools: ['0x26607ac599266b21d13c7acf7942c7701a8b699c', '0xb4bebd34f6daafd808f73de0d10235a92fbb6c3d'],
       }, {
-        YCRV: '0xdF5e0e81Dff6FAF3A7e52BA697820c5e32D806A8',
+        YCRV: yCrvAddr,
         USDC: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
         YFI: yfiAddr,
         uniswapRouter: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
@@ -78,6 +80,7 @@ task('deploy-yearn-router', 'Deploy Yearn Router')
       }),
       admin
     );
+    const yCrv = await MockERC20.at(yCrvAddr);
 
     console.log('yfi balance after', await callContract(token, 'balanceOf', [pool.address]));
     console.log('yfi wrapper balance after', await callContract(token, 'balanceOf', [wrappedToken.address]));
@@ -86,19 +89,23 @@ task('deploy-yearn-router', 'Deploy Yearn Router')
     await wrappedToken.pokeRouter();
     console.log('wrapped balance ratio 80%', await callContract(token, 'balanceOf', [wrappedToken.address]));
 
-    await router.setReserveConfig(ether(0.2), '3600', {from: admin});
-    await increaseTime(60 * 60 * 24);
+    // await router.setReserveConfig(ether(0.2), '3600', {from: admin});
+    await increaseTime(60 * 60 * 24 * 60);
     await wrappedToken.pokeRouter();
-    console.log('wrapped balance ratio 20%', await callContract(token, 'balanceOf', [wrappedToken.address]));
+    // console.log('wrapped balance ratio 20%', await callContract(token, 'balanceOf', [wrappedToken.address]));
 
-    // await increaseTime(60 * 60 * 24);
-    // await wrappedToken.pokeRouter();
+    const YearnGovernanceInterface = await artifacts.require('YearnGovernanceInterface');
+    const governance = await YearnGovernanceInterface.at(votingAddr);
+    console.log('staker.earned(wrappedToken.address)', await callContract(governance, 'earned', [wrappedToken.address]));
 
-    // const staker = await IStakedAave.at(stakingAddr);
-    // console.log('staker.balanceOf(wrappedToken.address)', await callContract(staker, 'balanceOf', [wrappedToken.address]));
-    // console.log('getUserAssetData', await callContract(staker, 'getUserAssetData', [wrappedToken.address, yfiAddr]));
-    // console.log('getTotalRewardsBalance', await callContract(staker, 'getTotalRewardsBalance', [wrappedToken.address]));
-    // console.log('stakerRewardsToClaim', await callContract(staker, 'stakerRewardsToClaim', [wrappedToken.address]));
+    console.log('balance of router before', await callContract(yCrv, 'balanceOf', [router.address]));
+    await router.claimRewards();
+    console.log('staker.earned(wrappedToken.address)', await callContract(governance, 'earned', [wrappedToken.address]));
+    console.log('wrapped balance before', await callContract(pool, 'getBalance', [wrappedToken.address]));
+    console.log('balance of router after', await callContract(yCrv, 'balanceOf', [router.address]));
+    await router.distributeRewards();
+    console.log('balance of router after 2', await callContract(yCrv, 'balanceOf', [router.address]));
+    console.log('wrapped balance after', await callContract(pool, 'getBalance', [wrappedToken.address]));
 
     function ether(amount) {
       return toWei(amount.toString(), 'ether');
