04f6e193db805f1064ae23499f9dd6849038ae05
defi-dev <defi.dev@protonmail.com>
2021-01-21 13:12:33 +0300

    fix PowerIndexPool gulp

--- contracts/balancer-core/BPool.sol
+++ contracts/balancer-core/BPool.sol
@@ -622,9 +622,13 @@ contract BPool is BToken, BMath, BPoolInterface {
         _lock_
     {
         _onlyWrapperOrNotWrapperMode();
-        uint256 diff = bsub(IERC20(token).balanceOf(address(this)), _records[token].balance);
-        if (diff > 0) {
-          IERC20(token).safeTransfer(_communityFeeReceiver, diff);
+        if (_records[token].bound) {
+          _records[token].balance = IERC20(token).balanceOf(address(this));
+        } else {
+          uint256 diff = bsub(IERC20(token).balanceOf(address(this)), _records[token].balance);
+          if (diff > 0) {
+            IERC20(token).safeTransfer(_communityFeeReceiver, diff);
+          }
         }
     }
 
