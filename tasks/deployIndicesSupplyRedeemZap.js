b555637d634b88625e3ed885ad1b9222b87b8b0e
defi-dev <defi.dev@protonmail.com>
2021-03-18 12:51:31 +0300

    migrate deploy-indices-supply-redeem-zap to reporter pokes

--- tasks/deployIndicesSupplyRedeemZap.js
+++ tasks/deployIndicesSupplyRedeemZap.js
@@ -3,9 +3,10 @@ require('@nomiclabs/hardhat-truffle5');
 const fs = require('fs');
 
 task('deploy-indices-supply-redeem-zap', 'Deploy Indices Supply Redeem Zap').setAction(async (__, {ethers, network}) => {
-  const {impersonateAccount, forkContractUpgrade, fromEther, mwei, fromMwei, deployProxied, callContract, increaseTime} = require('../test/helpers');
+  const {impersonateAccount, forkContractUpgrade, ether, fromEther, mwei, fromMwei, gwei, deployProxied, callContract, increaseTime} = require('../test/helpers');
   const IndicesSupplyRedeemZap = artifacts.require('IndicesSupplyRedeemZap');
   const PowerIndexPool = artifacts.require('PowerIndexPool');
+  const PowerPoke = await artifacts.require('PowerPoke');
 
   const { web3 } = IndicesSupplyRedeemZap;
 
@@ -52,10 +53,30 @@ task('deploy-indices-supply-redeem-zap', 'Deploy Indices Supply Redeem Zap').set
   const ERC20 = await artifacts.require('ERC20');
   const holder = '0xf977814e90da44bfa03b6295a0616a897441acec';
   const pool = await PowerIndexPool.at(poolAddress);
+  const pokerReporter = '0xabdf215fce6c5b0c1b40b9f2068204a9e7c49627';
+
+  const BONUS_NUMERATOR = '7610350076';
+  const BONUS_DENUMERATOR = '10000000000000000';
+  const MIN_REPORT_INTERVAL = 1;
+  const MAX_REPORT_INTERVAL = MIN_REPORT_INTERVAL + 60 * 60;
+  const MAX_GAS_PRICE = gwei(500);
+  const PER_GAS = '10000';
+  const MIN_SLASHING_DEPOSIT = ether(40);
 
   await impersonateAccount(ethers, admin);
   await impersonateAccount(ethers, holder);
+  await impersonateAccount(ethers, pokerReporter);
   await forkContractUpgrade(ethers, admin, proxyAdminAddr, poolAddress, await PowerIndexPool.new().then(r => r.address));
+
+  const powerPoke = await PowerPoke.at(powerPokeAddress);
+  await powerPoke.addClient(zap.address, admin, true, MAX_GAS_PRICE, MIN_REPORT_INTERVAL, MAX_REPORT_INTERVAL, {from: admin});
+  await powerPoke.setMinimalDeposit(zap.address, MIN_SLASHING_DEPOSIT, {from: admin});
+  await powerPoke.setBonusPlan(zap.address, '1', true, BONUS_NUMERATOR, BONUS_DENUMERATOR, PER_GAS, {from: admin});
+  await powerPoke.setFixedCompensations(zap.address, 200000, 60000, {from: admin});
+
+  const cvp = await PowerIndexPool.at('0x38e4adb44ef08f22f5b5b76a8f0c2d0dcbe7dca1');
+  await cvp.approve(powerPoke.address, ether(10000), {from: admin});
+  await powerPoke.addCredit(zap.address, ether(10000), {from: admin});
 
   const usdc = await ERC20.at(usdcAddress);
 
@@ -64,7 +85,7 @@ task('deploy-indices-supply-redeem-zap', 'Deploy Indices Supply Redeem Zap').set
   await usdc.approve(zap.address, mwei(100000), {from: holder});
   await zap.depositErc20(poolAddress, usdc.address, mwei(100000), {from: holder});
 
-  let roundKey = await callContract(zap, 'getCurrentRoundKey', [poolAddress, usdc.address, poolAddress]);
+  let roundKey = await callContract(zap, 'getLastRoundKey', [poolAddress, usdc.address, poolAddress]);
 
   await increaseTime(roundPeriod + 1);
 
@@ -72,11 +93,15 @@ task('deploy-indices-supply-redeem-zap', 'Deploy Indices Supply Redeem Zap').set
 
   console.log('usdc balance before supply', fromMwei(await callContract(usdc, 'balanceOf', [zap.address])));
 
-  await zap.supplyAndRedeemPoke([roundKey]);
+  const powerPokeOpts = web3.eth.abi.encodeParameter(
+    { PowerPokeRewardOpts: {to: 'address', compensateInETH: 'bool'} },
+    {to: pokerReporter, compensateInETH: false},
+  );
+  await zap.supplyAndRedeemPokeFromReporter('1', [roundKey], powerPokeOpts, {from: pokerReporter});
 
   console.log('usdc balance after supply', fromMwei(await callContract(usdc, 'balanceOf', [zap.address])));
 
-  await zap.claimPoke(roundKey, [holder]);
+  await zap.claimPokeFromReporter('1', roundKey, [holder], powerPokeOpts, {from: pokerReporter});
 
   const resultPoolBalance = await callContract(pool, 'balanceOf', [holder]);
   console.log('result pool balance', fromEther(resultPoolBalance));
@@ -93,18 +118,18 @@ task('deploy-indices-supply-redeem-zap', 'Deploy Indices Supply Redeem Zap').set
 
   const usdcBalanceBefore = fromMwei(await callContract(usdc, 'balanceOf', [holder]));
 
-  roundKey = await callContract(zap, 'getCurrentRoundKey', [poolAddress, poolAddress, usdc.address]);
+  roundKey = await callContract(zap, 'getLastRoundKey', [poolAddress, poolAddress, usdc.address]);
 
   await increaseTime(roundPeriod + 1);
   await usdc.approve(zap.address, mwei(100000), {from: holder});
 
   console.log('pool balance before redeem', fromEther(await callContract(pool, 'balanceOf', [zap.address])));
 
-  await zap.supplyAndRedeemPoke([roundKey]);
+  await zap.supplyAndRedeemPokeFromReporter('1', [roundKey], powerPokeOpts, {from: pokerReporter});
 
   console.log('pool balance after redeem', fromEther(await callContract(pool, 'balanceOf', [zap.address])));
 
-  await zap.claimPoke(roundKey, [holder]);
+  await zap.claimPokeFromReporter('1', roundKey, [holder], powerPokeOpts, {from: pokerReporter});
 
   console.log('result usdc balance', fromMwei(await callContract(usdc, 'balanceOf', [holder])) - usdcBalanceBefore);
 });
