9969703235c883b2bcf0c9a4a7d2a77589e09877
defi-dev <defi.dev@protonmail.com>
2021-06-05 11:26:09 +0300

    improve WeightValueChangeRateAbstract

--- contracts/weight-strategies/WeightValueChangeRateAbstract.sol
+++ contracts/weight-strategies/WeightValueChangeRateAbstract.sol
@@ -10,6 +10,12 @@ abstract contract WeightValueChangeRateAbstract is WeightValueAbstract {
   mapping(address => uint256) public lastValue;
   mapping(address => uint256) public valueChangeRate;
 
+  bool public rateChangeDisabled;
+
+  event UpdatePoolTokenValue(address indexed token, uint256 oldTokenValue, uint256 newTokenValue, uint256 lastChangeRate, uint256 newChangeRate);
+  event SetValueChangeRate(address indexed token, uint256 oldRate, uint256 newRate);
+  event SetRateChangeDisabled(bool rateChangeDisabled);
+
   constructor() public WeightValueAbstract() {}
 
   function _updatePoolByPoke(
@@ -24,11 +30,13 @@ abstract contract WeightValueChangeRateAbstract is WeightValueAbstract {
       lastValue[_tokens[i]] = _newTokenValues[i];
       console.log("_newTokenValues[i]", _newTokenValues[i]);
 
-      if (oldValue != 0) {
-        uint256 lastChangeRate = valueChangeRate[_tokens[i]] == 0 ? 1 ether : valueChangeRate[_tokens[i]];
+      uint256 lastChangeRate;
+      if (oldValue != 0 && !rateChangeDisabled) {
+        lastChangeRate = valueChangeRate[_tokens[i]] == 0 ? 1 ether : valueChangeRate[_tokens[i]];
         valueChangeRate[_tokens[i]] = bmul(bdiv(_newTokenValues[i], oldValue), lastChangeRate);
         console.log("valueChangeRate[_tokens[i]]", valueChangeRate[_tokens[i]]);
       }
+      emit UpdatePoolTokenValue(_tokens[i], oldValue, _newTokenValues[i], lastChangeRate, valueChangeRate[_tokens[i]]);
     }
   }
 
@@ -39,11 +47,18 @@ abstract contract WeightValueChangeRateAbstract is WeightValueAbstract {
     }
   }
 
-  function setValueChangeRate(address[] memory _tokens, uint256[] memory _newTokenRates) public onlyOwner {
+  function setValueChangeRates(address[] memory _tokens, uint256[] memory _newTokenRates) public onlyOwner {
     uint256 len = _tokens.length;
     require(len == _newTokenRates.length, "LENGTHS_MISMATCH");
     for (uint256 i = 0; i < len; i++) {
+      emit SetValueChangeRate(_tokens[i], valueChangeRate[_tokens[i]], _newTokenRates[i]);
+
       valueChangeRate[_tokens[i]] = _newTokenRates[i];
     }
+  }
+
+  function setRateUpdateDisabled(bool _disabled) public onlyOwner {
+    rateChangeDisabled = _disabled;
+    emit SetRateChangeDisabled(rateChangeDisabled);
   }
 }
