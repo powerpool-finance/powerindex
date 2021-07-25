cd632055ac2ca08aab84a6d4fa65e6c47ebf86a2
defi-dev <defi.dev@protonmail.com>
2021-07-25 03:47:24 +0300

    add SafeERC20 to xCVP

--- contracts/pvp/xCVP.sol
+++ contracts/pvp/xCVP.sol
@@ -4,10 +4,12 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract xCVP is ERC20("Permanent Voting Power Token", "xCVP") {
   using SafeMath for uint256;
+  using SafeERC20 for IERC20;
   IERC20 public immutable cvp;
 
   constructor(IERC20 cvp_) public {
@@ -23,14 +25,14 @@ contract xCVP is ERC20("Permanent Voting Power Token", "xCVP") {
       uint256 what = _amount.mul(totalShares).div(totalCVP);
       _mint(msg.sender, what);
     }
-    cvp.transferFrom(msg.sender, address(this), _amount);
+    cvp.safeTransferFrom(msg.sender, address(this), _amount);
   }
 
   function leave(uint256 _share) public {
     uint256 totalShares = totalSupply();
     uint256 what = _share.mul(cvp.balanceOf(address(this))).div(totalShares);
     _burn(msg.sender, _share);
-    cvp.transfer(msg.sender, what);
+    cvp.safeTransfer(msg.sender, what);
   }
 
   function name() public view override returns (string memory) {
