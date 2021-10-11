9cad645b9a62301d24dd735f3642be0def41ab31
Poli Paul <polipaul777@hotmail.com>
2021-10-11 17:54:16 +0200

    Routers/Bakery: Add a notice on the staking contract signature specifics

--- contracts/powerindex-router/implementations/BakeryChefPowerIndexRouter.sol
+++ contracts/powerindex-router/implementations/BakeryChefPowerIndexRouter.sol
@@ -10,6 +10,8 @@ import "./AbstractMasterChefIndexRouter.sol";
  * Compatible with:
  * - Bakery: https://bscscan.com/address/0x20ec291bb8459b6145317e7126532ce7ece5056f,
  *   pending rewards via pendingBake(pair, user)
+ * @dev Notice that in deposit/withdraw/pendingBake method signatures Bakery uses the staking token addresses
+ *      instead of numerical pool IDs like in the majority of masterChef forks.
  */
 contract BakeryChefPowerIndexRouter is AbstractMasterChefIndexRouter {
   struct BakeryMasterChefConfig {
