9a3c3e3323b7d78d40fe5c2c3737bd132768c859
defibrain88 <defibrain88@proton.me>
2024-02-06 18:10:16 +0700

    feat: made a tvl calculation for a pool with id 0

--- contracts/powerindex-mining/PoolsLens.sol
+++ contracts/powerindex-mining/PoolsLens.sol
@@ -11,6 +11,17 @@ import "hardhat/console.sol";
     uint256 accCvpPerLpt; // accumulated distributed CVPs per one deposited LP token, times 1e12
   }
 
+  struct LpData {
+    uint256 tvl;
+    uint256 apy;
+  }
+
+  struct ReservesStruct {
+    uint112 reserve0;
+    uint112 reserve1;
+    uint32 blockTimestampLast;
+  }
+
 interface IUniswapV2Router {
   function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
 }
@@ -19,6 +30,10 @@ interface IVestedLpMining {
   function pools(uint256 index) external view returns (Pool calldata);
 
   function poolLength() external view returns(uint);
+}
+
+interface ILpToken {
+  function getReserves() external view returns (ReservesStruct calldata);
 }
 
 contract PoolsLens {
@@ -47,8 +62,39 @@ contract PoolsLens {
     return pools;
   }
 
-  function getPoolEsentialData(uint8 poolId) external view returns (Pool memory) {
+  function getPoolData(uint8 poolId) external view returns (LpData memory) {
     Pool[] memory pools = getPools();
-    return pools[poolId];
+    Pool memory pool = pools[poolId];
+
+    if (poolId == 0) {
+      // tvl calculation
+      ReservesStruct memory reserves = ILpToken(pool.lpToken).getReserves();
+      address[] memory path = new address[](3);
+      address cvpAddress = 0x38e4adB44ef08F22F5B5b76A8f0c2d0dCbE7DcA1;
+      address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+      address usdcAddress = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
+      address[] memory cvpPath = new address[](3);
+      cvpPath[0] = cvpAddress;
+      cvpPath[1] = wethAddress;
+      cvpPath[2] = usdcAddress;
+      address[] memory ethPath = new address[](2);
+      ethPath[0] = wethAddress;
+      ethPath[1] = usdcAddress;
+
+      uint256 cvpPrice = uniRouter.getAmountsOut(1 ether, cvpPath)[2];
+      uint256 ethPrice = uniRouter.getAmountsOut(1 ether, ethPath)[1];
+
+      uint256 tvl = ((reserves.reserve0 * cvpPrice) + (reserves.reserve1 * ethPrice));
+
+      // apy calculation
+      return LpData({
+        tvl: tvl / 10 ** 6 / 1 ether,
+        apy: 0
+      });
+    }
+    return LpData({
+      tvl: 0,
+      apy: 0
+    });
   }
 }
--- test/PoolsLens.test.js
+++ test/PoolsLens.test.js
@@ -22,7 +22,7 @@ describe.only('PoolsLens', async () => {
           '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
           '0xdAC17F958D2ee523a2206206994597C13D831ec7',
       );
-      const zeroPool = await this.poolsLens.getPoolEsentialData(0);
+      const zeroPool = await this.poolsLens.getPoolData(0);
       console.log('zero pool info is: ', zeroPool);
     });
   } catch (e) {
