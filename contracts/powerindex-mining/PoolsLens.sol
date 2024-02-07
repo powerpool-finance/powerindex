e5f03b8bf82127ae39a75ae6373a25fa7c0b8a9f
defibrain88 <defibrain88@proton.me>
2024-02-07 11:32:35 +0700

    feat: made an apy calculation for a pool with id 0

--- contracts/powerindex-mining/PoolsLens.sol
+++ contracts/powerindex-mining/PoolsLens.sol
@@ -1,6 +1,17 @@
-pragma solidity 0.8.11;
+/*
+https://powerpool.finance/
+          wrrrw r wrr
+         ppwr rrr wppr0       prwwwrp                                 prwwwrp                   wr0
+        rr 0rrrwrrprpwp0      pp   pr  prrrr0 pp   0r  prrrr0  0rwrrr pp   pr  prrrr0  prrrr0    r0
+        rrp pr   wr00rrp      prwww0  pp   wr pp w00r prwwwpr  0rw    prwww0  pp   wr pp   wr    r0
+        r0rprprwrrrp pr0      pp      wr   pr pp rwwr wr       0r     pp      wr   pr wr   pr    r0
+         prwr wrr0wpwr        00        www0   0w0ww    www0   0w     00        www0    www0   0www0
+          wrr ww0rrrr
+*/
 
-import "hardhat/console.sol";
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.11;
 
   struct Pool {
     address lpToken; // address of the LP token contract
@@ -28,6 +39,8 @@ interface IUniswapV2Router {
 
 interface IVestedLpMining {
   function pools(uint256 index) external view returns (Pool calldata);
+  function cvpPerBlock() external view returns (uint96);
+  function totalAllocPoint() external view returns (uint256);
 
   function poolLength() external view returns(uint);
 }
@@ -46,7 +59,7 @@ contract PoolsLens {
     IVestedLpMining _mining,
     IUniswapV2Router _router,
     address _usdtAddress
-  ) public {
+  ) {
     mining = _mining;
     uniRouter = _router;
     usdtAddress = _usdtAddress;
@@ -67,29 +80,31 @@ contract PoolsLens {
     Pool memory pool = pools[poolId];
 
     if (poolId == 0) {
+      uint256 stableDecimals = 10 ** 6;
       // tvl calculation
       ReservesStruct memory reserves = ILpToken(pool.lpToken).getReserves();
-      address[] memory path = new address[](3);
-      address cvpAddress = 0x38e4adB44ef08F22F5B5b76A8f0c2d0dCbE7DcA1;
-      address wethAddress = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-      address usdcAddress = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
       address[] memory cvpPath = new address[](3);
-      cvpPath[0] = cvpAddress;
-      cvpPath[1] = wethAddress;
-      cvpPath[2] = usdcAddress;
+      cvpPath[0] = 0x38e4adB44ef08F22F5B5b76A8f0c2d0dCbE7DcA1;
+      cvpPath[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+      cvpPath[2] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
       address[] memory ethPath = new address[](2);
-      ethPath[0] = wethAddress;
-      ethPath[1] = usdcAddress;
+      ethPath[0] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+      ethPath[1] = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
 
-      uint256 cvpPrice = uniRouter.getAmountsOut(1 ether, cvpPath)[2];
-      uint256 ethPrice = uniRouter.getAmountsOut(1 ether, ethPath)[1];
+      uint256 cvpPrice = (uniRouter.getAmountsOut(1 ether, cvpPath)[2]);
+      uint256 ethPrice = (uniRouter.getAmountsOut(1 ether, ethPath)[1]);
 
-      uint256 tvl = ((reserves.reserve0 * cvpPrice) + (reserves.reserve1 * ethPrice));
-
+      uint256 tvlInUsd = ((reserves.reserve0 * cvpPrice) + (reserves.reserve1 * ethPrice)) / 1 ether / stableDecimals;
       // apy calculation
+      uint256 blocksPerYear = 365 * 7100;
+      uint256 cvpPerBlock = mining.cvpPerBlock();
+      uint256 poolWeight = ((pool.allocPoint * stableDecimals) / mining.totalAllocPoint());
+      uint256 apy = (blocksPerYear * cvpPerBlock * poolWeight * cvpPrice / tvlInUsd) / (10 ** (6 * 4));
+
+      // to get whole apy percents you need to do: apy / 10 ** 5
       return LpData({
-        tvl: tvl / 10 ** 6 / 1 ether,
-        apy: 0
+        tvl: tvlInUsd,
+        apy: apy
       });
     }
     return LpData({
